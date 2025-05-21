fca5fc4c18dbe4e6a1923a9c9629fb7f3cde0d6e
RafaelBertani <Rafaelm.bertani@gmail.com>
2025-05-21 18:15:19 -0300

    fix: corrige funcionamento das bombas

--- main.py
+++ main.py
@@ -7,14 +7,21 @@ trigger = Pin(16, Pin.OUT)
 echo = Pin(17, Pin.IN)
 
 botao = Pin(19, Pin.IN, Pin.PULL_UP)
-bomba_entrada = Pin(15, Pin.OUT)
+bomba_entrada = Pin(14, Pin.OUT)
 bomba_saida = Pin(12, Pin.OUT)
 
 bomba_entrada_ativa = False
 bomba_saida_ativa = False
 
+bomba_entrada.low()
+bomba_saida.low()
+
 # Função para medir distância com o HC-SR04
 def ultrassom():
+    
+    global bomba_entrada_ativa
+    global bomba_saida_ativa
+    
     trigger.low()
     utime.sleep_us(2)
     trigger.high()
@@ -36,22 +43,26 @@ def ultrassom():
     timepassed = signalon - signaloff
     distance = (timepassed * 0.0343) / 2
     print("Distância até o objeto: {:.2f} cm".format(distance))
-    
+        
     if distance>10 and bomba_saida_ativa==False:
         bomba_entrada_ativa=True
-        bomba_entrada.high()
-    else:
         bomba_entrada.low()
+    else:
+        bomba_entrada.high()
         bomba_entrada_ativa=False
 
-# Função que roda em outra thread para o botão e LED
+#Função que roda em outra thread para o botão e LED
 def botao_bomba_saida():
+    
+    global bomba_entrada_ativa
+    global bomba_saida_ativa
+    
     while True:
         if not botao.value() and bomba_entrada_ativa==False:  # Botão pressionado (ativo em nível baixo)
             bomba_saida_ativa=True
-            bomba_saida.high()
-        else:
             bomba_saida.low()
+        else:
+            bomba_saida.high()
             bomba_saida_ativa=False
         utime.sleep(0.05)  # Pequeno delay para evitar rebote
 
