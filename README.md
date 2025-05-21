0e6606ef76a1c597eb3014c2c723f8b0573b428a
Igor Improta <igorims2003@gmail.com>
2025-05-21 16:45:47 -0300

    feat: adiciona esqueleto do projeto

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # microcontroladores-s1
  Trabalho Semestral de Microcontroladores, pelo Instituto Mauá de Tecnologia
+
+ Descrição do projeto:
+
+ Bombas d'água controladas por raspberry py pico que enchem e esvaziam tanque
+ de acordo com a altura de água, medida por ultrassom.
+
+ Integrantes:
+ Giuliano Rodrigues Tumoli  21.00036-0
+ Igor Improta M. da Silva   21.00834-5
+ Rafael Bertani             21.00634-2
---
+++ main.py
@@ -0,0 +1,64 @@
+from machine import Pin
+import utime
+import _thread
+
+# Ultrassom: Trigger no GP16, Echo no GP17
+trigger = Pin(16, Pin.OUT)
+echo = Pin(17, Pin.IN)
+
+botao = Pin(19, Pin.IN, Pin.PULL_UP)
+bomba_entrada = Pin(15, Pin.OUT)
+bomba_saida = Pin(12, Pin.OUT)
+
+bomba_entrada_ativa = False
+bomba_saida_ativa = False
+
+# Função para medir distância com o HC-SR04
+def ultrassom():
+    trigger.low()
+    utime.sleep_us(2)
+    trigger.high()
+    utime.sleep_us(10)
+    trigger.low()
+    
+    distance=0
+
+    # Espera o início do pulso de eco
+    while echo.value() == 0:
+        pass
+    signaloff = utime.ticks_us()
+
+    # Espera o fim do pulso de eco
+    while echo.value() == 1:
+        pass
+    signalon = utime.ticks_us()
+
+    timepassed = signalon - signaloff
+    distance = (timepassed * 0.0343) / 2
+    print("Distância até o objeto: {:.2f} cm".format(distance))
+    
+    if distance>10 and bomba_saida_ativa==False:
+        bomba_entrada_ativa=True
+        bomba_entrada.high()
+    else:
+        bomba_entrada.low()
+        bomba_entrada_ativa=False
+
+# Função que roda em outra thread para o botão e LED
+def botao_bomba_saida():
+    while True:
+        if not botao.value() and bomba_entrada_ativa==False:  # Botão pressionado (ativo em nível baixo)
+            bomba_saida_ativa=True
+            bomba_saida.high()
+        else:
+            bomba_saida.low()
+            bomba_saida_ativa=False
+        utime.sleep(0.05)  # Pequeno delay para evitar rebote
+
+# Iniciar a thread paralela para o botão
+_thread.start_new_thread(botao_bomba_saida, ())
+
+# Loop principal com sensor ultrassônico
+while True:
+    ultrassom()
+    utime.sleep(1)
